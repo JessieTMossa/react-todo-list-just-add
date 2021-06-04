f663b034f9db3c897562c57fcdc5a9f0868e5a4d
JessieTMossa <76855680+JessieTMossa@users.noreply.github.com>
2021-06-03 20:38:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+// import useState hook, Header component, List component, AddTodo component
 import { useState } from 'react'
 import Header from "./components/Header";
 import List from "./components/List"
 import AddTodo from "./components/AddTodo"
 
+// create function 
 function App() {
-
+// create variable using array destructuring, useState hook holding array of objects, each with unique id
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -20,9 +22,11 @@ function App() {
     },
   ]);
 
+//  create variable addTodo event handler using argument todo, generates random id#,
   const addTodo = (todo) => {
     const id = Math.ceil(Math.random()*100000)
     const newTodo = {id, ...todo}
+//     combines existing todos and new todos in an array
     setTodos([...todos, newTodo])
   }
 

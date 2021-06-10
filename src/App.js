39b4125a1dd067a6fa5966250d298d7f2c01d59a
JessieTMossa <76855680+JessieTMossa@users.noreply.github.com>
2021-06-09 19:32:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import AddTodo from "./components/AddTodo"
 
 // create function 
 function App() {
-// create variable using array destructuring, useState hook holding array of objects, each with unique id
+// create global state variable using array destructuring, useState hook holding array of objects, each with unique id
+//  useState hook gives us 2 pieces of info at beginning: actual state (todos here), and a way to change the state (setTodos here)
   const [todos, setTodos] = useState([
+//     whatever is inside useState parentheses (here an array of objects/todos) will be initial state
     {
       id: 1,
       item: "Drink Water",
@@ -22,7 +24,7 @@ function App() {
     },
   ]);
 
-//  create variable addTodo event handler using argument todo, generates random id#,
+//  create addTodo event handler method using argument todo, generates random id#, helps work with state
   const addTodo = (todo) => {
     const id = Math.ceil(Math.random()*100000)
     const newTodo = {id, ...todo}
@@ -30,13 +32,18 @@ function App() {
     setTodos([...todos, newTodo])
   }
 
+//    return rendered HTML of the individual components.
   return (
     <div className="container">
+//     title is prop for Header component
       <Header title="Todo List" />
+//     input form for new todos
       <AddTodo onAdd={addTodo} />
+// list component has child component of individual todos
       <List todos={todos}/>
     </div>
   );
 }
 
+// export App so it can be accessed in index.js
 export default App;

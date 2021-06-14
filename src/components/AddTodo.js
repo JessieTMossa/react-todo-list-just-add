1f83947eab10080056f0c0b54436a4dcc17c97da
JessieTMossa <76855680+JessieTMossa@users.noreply.github.com>
2021-06-14 18:28:25 -0500

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,27 +1,34 @@
+// import React useState hook
+import { useState } from 'react' 
 
-import { useState } from 'react' // import React useState hook
-
-const AddTodo = ({ onAdd }) => { // define constant variable called AddTodo, uses onAdd event handler as prop (similar to onClick) via destructuring
-    const [item, setItem] = useState('')//    item value entered by user, empty string default value of new ToDo 
-    const onSubmit = (e) => { // new handler function, e is event, preventDefault asks page not to refresh
+// define constant variable called AddTodo, uses onAdd event handler as prop (similar to onClick) via destructuring
+const AddTodo = ({ onAdd }) => { 
+    //    item value entered by user, empty string default value of new ToDo 
+    const [item, setItem] = useState('')
+    // new handler function, e is event, preventDefault asks page not to refresh
+    const onSubmit = (e) => { 
         e.preventDefault()
         onAdd({item})
 
         setItem('')
     }
 
+//     return rendered html of Todo form 
     return (
         <form onSubmit={onSubmit}>
             <label>Add Todo:</label>
+// form input, textbox with placeholder text, assigns entered text to {item}, when text is entered (onChange), updates setItem (from line 7)
             <input 
                 type="text" 
                 placeholder="Todo Item"
                 value={item}
                 onChange={ (e) => setItem(e.target.value)}
             /> 
+// form submit button labeled "Add Todo"
             <input type="submit" value="Add Todo" />
         </form>
     )
 }
 
+// export AddTodo component for use in App.js
 export default AddTodo
